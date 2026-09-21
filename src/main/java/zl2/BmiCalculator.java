d85c5c3ff07536f6b7a494a836239571a178ce1d
laban <plabanowski9@gmail.com>
2026-09-21 22:42:35 +0200

    ZL-2: fixes after CodeReview

--- src/main/java/zl2/BmiCalculator.java
+++ src/main/java/zl2/BmiCalculator.java
@@ -6,27 +6,30 @@ public class BmiCalculator {
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
         System.out.println("Please enter your weight in kg");
-        double weight = sn.nextDouble();
+        double weightInKg = sn.nextDouble();
         System.out.println("Please enter your height in cm");
-        double height = sn.nextDouble();
-        double heightInMeters = height*0.01;
-        double BMI = weight/(heightInMeters*heightInMeters);
-        System.out.println("Your BMI is ");
-        System.out.printf("%.2f%n", BMI);
+        double heightInCm = sn.nextDouble();
+        double heightInMeters = heightInCm * 0.01;
+        double bmi = weightInKg / (heightInMeters * heightInMeters);
+        System.out.println("Your bmi is ");
+        System.out.printf("%.2f%n", bmi);
 
-        if(BMI>0 && BMI<18.5){
+        if (bmi <= 0) {
+            System.out.println("BMI can't be less or equals 0!");
+        } else if (bmi > 0 && bmi < 18.5) {
             System.out.println("Underweight");
-        } else if (BMI >= 18.5 && BMI <= 24.9) {
+        } else if (bmi >= 18.5 && bmi < 25.0) {
             System.out.println("Normal weight");
-        } else if (BMI >= 25.0 && BMI <= 29.9) {
+        } else if (bmi >= 25.0 && bmi < 30.0) {
             System.out.println("Overweight");
-        } else if (BMI >= 30.0) {
+        } else if (bmi >= 30.0) {
             System.out.println("Obesity");
         } else {
             System.out.println("Something is wrong with your inputs, try again");
         }
-        if (height <30 || height >250) {
+        if (heightInCm < 30 || heightInCm > 250) {
             System.out.println("Warring! You are not human?");
         }
+        sn.close();
     }
 }
